3c7948ce888dc75dda678307b5c1c83c41c5c941
Sarah F. Graves <s.graves@eaobservatory.org>
2017-10-13 10:08:39 -1000

    update script now takes start and end dates

--- papertracking-update.py
+++ papertracking-update.py
@@ -2,14 +2,16 @@
 
 
 """
-Usage: papertracking-update.py [-v] <searchid> [--dryrun]
+Usage: papertracking-update.py [-v] <searchid> [--dryrun] [--start=<start>] [--end=<end>]
 
 PaperTracking: Update the ADS search with the given ID.
 
 Options:
-  -h --help       Show this help.
-  -v --verbose    DEBUG logging level
-  -d --dryrun     Run query but don't update DB
+  -h --help          Show this help.
+  -v --verbose       DEBUG logging level
+  -d --dryrun        Run query but don't update DB
+  -s --start=<start> Start date for query (YYYY-MM)
+  -e --end=<end>     End date for query (YYYY-MM)
 """
 
 from docopt import docopt
@@ -17,6 +19,7 @@ from docopt import docopt
 from papertracking.util import create_session
 from papertracking import query
 import logging
+import datetime
 
 arguments = docopt(__doc__, help=True)
 
@@ -27,7 +30,12 @@ else:
 
 searchid = arguments['<searchid>']
 dryrun = arguments['--dryrun']
-
+end = arguments['--end']
+start = arguments['--start']
+if start:
+    start = datetime.datetime.strptime(start, '%Y-%m')
+if end:
+    end = datetime.datetime.strptime(end, '%Y-%m')
 
 session = create_session()
-query.update_search(1, session, dryrun=dryrun)
+query.update_search(1, session, dryrun=dryrun, startdate=start, enddate=end)
